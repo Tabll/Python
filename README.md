88026e606988f158d273ce85d28ab873181898de
Tabll <905153840@qq.com>
2018-08-12 16:21:05 +0100

    初始化仓库

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Python
-我的私人Python代码仓库
+Python 代码仓库
+
+## 版本与环境
+Python 版本 `3.6.6` </br>
+TensorFlow 版本 `1.10.0`
+
+### 更新时间
+2018年8月
---
+++ Tools/Update All.py
@@ -0,0 +1,4 @@
+from pip._internal.utils.misc import get_installed_distributions
+from subprocess import call
+for dist in get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
