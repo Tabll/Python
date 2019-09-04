585b6004b011458f4fca4ee9eb3b6628be13020e
Tabll <905153840@qq.com>
2019-09-04 23:48:32 +0800

    Tensorflow初始化

---
+++ Nodes/Tensorflow介绍.md
@@ -0,0 +1,12 @@
+# TensorFlow
+[主要来源](https://time.geekbang.org/course/intro/153)*极客时间 彭靖田*  
+人工智能从16大以来提到了国家战略
+
+## TensorFlow 印象
+## TensorFlow 接触
+## TensorFlow 概念
+## TensorFlow 房价预测
+## TensorFlow 手写体数字识别
+## TensorFlow 验证码数字识别
+## TensorFlow 人脸识别
+## TensorFlow 社区参与指南
--- Tools/Update All.py
+++ Tools/Update All.py
@@ -1,4 +1,4 @@
 from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
 for dist in get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("pip3.6 install --upgrade " + dist.project_name, shell=True)
